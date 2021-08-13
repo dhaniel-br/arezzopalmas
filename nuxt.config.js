9db1a791fdacac2bd5cd38fd42e1a258096de71b
Dhaniel Oliveira <dhanieloliveiracoelho@gmail.com>
2021-08-13 09:27:55 -0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,17 +11,14 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['normalize.css/normalize.css','@assets/scss/base.scss'],
+  css: ['normalize.css/normalize.css', '@assets/scss/base.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: '@/components', pathPrefix: false }],
@@ -38,14 +35,13 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     '@nuxtjs/style-resources',
-    '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module'
   ],
   styleResources: {
     // your settings here
-    scss: ['@/components/bosons/*.scss'],
-   },
+    scss: ['@/components/bosons/*.scss']
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
